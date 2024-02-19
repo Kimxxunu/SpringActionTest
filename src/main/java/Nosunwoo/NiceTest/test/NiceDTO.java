27414bf5f1790fb2151deb2124b0cdae4fcb3321
sunwoo <114386406+Kimxxunu@users.noreply.github.com>
2024-02-19 17:07:52 +0900

    Fixes : codedeploy 파일 수정5 ~

--- src/main/java/Nosunwoo/NiceTest/test/NiceDTO.java
+++ src/main/java/Nosunwoo/NiceTest/test/NiceDTO.java
@@ -9,10 +9,10 @@ import lombok.Setter;
 @Builder
 
 public class NiceDTO {
-    private String name = "마동석";
+    private String name = "김민재";
     private int number = 1004;
-    private String location = "춘천시 후평동";
-    private String fruit = "apple";
+    private String location = "남양주시 덕소리";
+    private String fruit = "banana";
     private String animal = "대머리독수리";
 
 }
