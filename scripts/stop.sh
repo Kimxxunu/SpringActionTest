2abbaff20c6f8729cf5b15b91705c3ba3ddb61ae
sunwoo <114386406+Kimxxunu@users.noreply.github.com>
2024-02-21 03:19:10 +0900

    Fix : 파일 수정46

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
-PROJECT_ROOT=""
+PROJECT_ROOT="/home/ubuntu"
 JAR_FILE="$PROJECT_ROOT/NiceTest-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
@@ -6,7 +6,7 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인1
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f NiceTest-0.0.1-SNAPSHOT.jar)
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
