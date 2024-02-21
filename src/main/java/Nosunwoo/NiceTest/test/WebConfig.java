9332857ecd4dc6b8d5b27a1974b92a995d392939
sunwoo <114386406+Kimxxunu@users.noreply.github.com>
2024-02-22 00:56:14 +0900

    Fix : 대성이랑 웹소켓  테스트 수정6

--- src/main/java/Nosunwoo/NiceTest/test/WebConfig.java
+++ src/main/java/Nosunwoo/NiceTest/test/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000") // 허용할 출처 추가
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱1
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2
     }
 }
