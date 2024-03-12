866a7f3742ae42a1403ad745c0bfe3a094e67ee4
sunwoo <114386406+Kimxxunu@users.noreply.github.com>
2024-03-12 21:36:23 +0900

    Fix : 대성이 cors 수정함2

--- src/main/java/Nosunwoo/NiceTest/test/WebConfig.java
+++ src/main/java/Nosunwoo/NiceTest/test/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "http://github-action-chatting-app.s3-website.ap-northeast-2.amazonaws.com/") // 허용할 출처 추가
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "http://github-action-chatting-app.s3-website.ap-northeast-2.amazonaws.com/","ws://43.202.160.134:8080") // 허용할 출처 추가
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2
