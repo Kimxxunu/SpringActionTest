e24c0cd0a84208e3fa7bf2c7a38dbff810e48e9e
선우 데스크탑 <sam11222@naver.com>
2024-03-24 00:54:01 +0900

    Feat: 채팅 디비 추가4

--- src/main/java/Nosunwoo/NiceTest/test/chatting/repository/UsersRepository.java
+++ src/main/java/Nosunwoo/NiceTest/test/chatting/repository/UsersRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<UsersEntity, Integer> {
     UsersEntity findByUserName(String userName);
+    //
 }
