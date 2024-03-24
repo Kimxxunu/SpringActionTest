14849a81f6edc67a4e32e6508f57ae8ac739396e
sunwoo <114386406+Kimxxunu@users.noreply.github.com>
2024-03-24 18:05:13 +0900

    Fix : 채팅 기능 업그레이드5

--- src/main/java/Nosunwoo/NiceTest/test/chatting/controller/WebSocketController.java
+++ src/main/java/Nosunwoo/NiceTest/test/chatting/controller/WebSocketController.java
@@ -3,9 +3,7 @@ package Nosunwoo.NiceTest.test.chatting.controller;
 import Nosunwoo.NiceTest.test.chatting.dto.ChattingDto;
 import Nosunwoo.NiceTest.test.chatting.entity.ChatRoomEntity;
 import Nosunwoo.NiceTest.test.chatting.entity.UsersEntity;
-import Nosunwoo.NiceTest.test.chatting.service.UsersService;
-import Nosunwoo.NiceTest.test.chatting.service.ChatRoomService;
-import Nosunwoo.NiceTest.test.chatting.service.ChatRoomJoinService;
+import Nosunwoo.NiceTest.test.chatting.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -16,13 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class WebSocketController {
 
     private final SimpMessagingTemplate messagingTemplate;
-    private final UsersService usersService;
-    private final ChatRoomService chatRoomService;
-    private final ChatRoomJoinService chatRoomJoinService;
+    private final UsersServiceImpl usersService;
+    private final ChatRoomServiceImpl chatRoomService;
+    private final ChatRoomJoinServiceImpl chatRoomJoinService;
 
     @Autowired
-    public WebSocketController(SimpMessagingTemplate messagingTemplate, UsersService usersService,
-                               ChatRoomService chatRoomService, ChatRoomJoinService chatRoomJoinService) {
+    public WebSocketController(SimpMessagingTemplate messagingTemplate, UsersServiceImpl usersService,
+                               ChatRoomServiceImpl chatRoomService, ChatRoomJoinServiceImpl chatRoomJoinService) {
         this.messagingTemplate = messagingTemplate;
         this.usersService = usersService;
         this.chatRoomService = chatRoomService;
@@ -37,8 +35,8 @@ public class WebSocketController {
         // 채팅 방 정보 저장
         ChatRoomEntity chatRoomEntity = chatRoomService.saveChatRoom(chattingDto.getRoomName());
 
-//        // 채팅 방 참여 정보 저장
-//        chatRoomJoinService.saveChatRoomJoin(usersEntity, chatRoomEntity);
+        // 채팅 방 참여 정보 저장
+        chatRoomJoinService.saveChatRoomJoin(usersEntity, chatRoomEntity);
     }
 
 
