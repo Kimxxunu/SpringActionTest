13fd6b96a498157a6aef35f0586b7163cf8adcdf
sunwoo <114386406+Kimxxunu@users.noreply.github.com>
2024-03-24 18:22:22 +0900

    Fix : 채팅 기능 업그레이드7

--- src/main/java/Nosunwoo/NiceTest/test/chatting/controller/WebSocketController.java
+++ src/main/java/Nosunwoo/NiceTest/test/chatting/controller/WebSocketController.java
@@ -9,8 +9,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WebSocketController {
 
     private final SimpMessagingTemplate messagingTemplate;
