724e4f9dafb4d2eb8ef6c6cce2aa82c554b35a7d
선우 데스크탑 <sam11222@naver.com>
2024-05-02 04:56:46 +0900

    feat : s3 테스트파일 추가했음4

--- src/main/java/Nosunwoo/NiceTest/NiceTestApplication.java
+++ src/main/java/Nosunwoo/NiceTest/NiceTestApplication.java
@@ -3,11 +3,7 @@ package Nosunwoo.NiceTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-})
+@SpringBootApplication
 public class NiceTestApplication {
 
 	public static void main(String[] args) {
