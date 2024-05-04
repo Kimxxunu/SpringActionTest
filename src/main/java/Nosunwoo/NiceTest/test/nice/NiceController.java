9a409a9512b90bbb6dfb76b270e0a16f8732b5b2
선우 데스크탑 <sam11222@naver.com>
2024-05-05 01:47:54 +0900

    feat: 초기세팅 파일 업로드

--- src/main/java/Nosunwoo/NiceTest/test/nice/NiceController.java
+++ src/main/java/Nosunwoo/NiceTest/test/nice/NiceController.java
@@ -1,12 +1,23 @@
 package Nosunwoo.NiceTest.test.nice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api")
 public class NiceController {
+
+    public static final UUID randomUUID = UUID.randomUUID();
+
+    @GetMapping("/test")
+    public ResponseEntity<UUID> test(){
+        return ResponseEntity.ok(randomUUID);
+    }
+
 
     @GetMapping("/nice")
     public NiceDTO getNiceDTO() {
