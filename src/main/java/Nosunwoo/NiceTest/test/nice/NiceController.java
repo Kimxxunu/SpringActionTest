f0996f620573c81db6cddcbd7a4c3501fddabda4
선우 데스크탑 <sam11222@naver.com>
2024-05-07 21:30:55 +0900

    feat: 블루/그린 배포 완료

--- src/main/java/Nosunwoo/NiceTest/test/nice/NiceController.java
+++ src/main/java/Nosunwoo/NiceTest/test/nice/NiceController.java
@@ -15,27 +15,23 @@ import java.util.UUID;
 @RequestMapping("/api")
 public class NiceController {
 
-    public static final UUID randomUUID = UUID.randomUUID();
-
+    // application.yml에 설정된 값들을 주입받습니다.
     @Value("${server.env}")
-    private String env;
+    private String env; // 서버 환경
 
     @Value("${server.port}")
-    private String serverPort;
+    private String serverPort; // 서버 포트
 
     @Value("${server.serverAddress}")
-    private String serverAddress;
+    private String serverAddress; // 서버 주소
 
     @Value("${serverName}")
-    private String serverName;
+    private String serverName; // 서버 이름
 
-
-    @GetMapping("/test")
-    public ResponseEntity<UUID> test(){
-        return ResponseEntity.ok(randomUUID);
-    }
+    // 서버의 상태를 확인하는 엔드포인트
     @GetMapping("/hc")
     public  ResponseEntity<?> healthCheck(){
+        // 서버의 상태 정보를 Map에 담아 반환합니다.
         Map<String, String> responseData = new TreeMap<>();
         responseData.put("serverName", serverName);
         responseData.put("serverAddress", serverAddress);
@@ -45,10 +41,10 @@ public class NiceController {
         return ResponseEntity.ok(responseData);
     }
 
+    // 서버 환경을 반환하는 엔드포인트
     @GetMapping("/env")
     public  ResponseEntity<?> getEnv(){
-        Map<String, String> responseData = new HashMap<>();
-
+        // 서버의 환경 정보를 단일 값으로 반환합니다.
         return ResponseEntity.ok(env);
     }
 
